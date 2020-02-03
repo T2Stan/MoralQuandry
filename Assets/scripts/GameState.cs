7a7ad46381b03e93a8562da0e07fa7bbab5912d5
T2Stan <60527563+T2Stan@users.noreply.github.com>
2020-02-02 16:28:04 -0800

    Fix for return to main menu.

--- Assets/scripts/GameState.cs
+++ Assets/scripts/GameState.cs
@@ -226,6 +226,11 @@ public class GameState : MonoBehaviour
     
     public void ReturnToMainMenu()
     {
+        var menuUi = GameObject.Find("Menu UI");
+        if (menuUi != null)
+        {
+           Destroy(menuUi);
+        }
         SceneLoader.LoadScene("MainMenu", Texture2D.blackTexture);
     }
 }
